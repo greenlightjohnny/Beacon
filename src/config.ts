18a6080a7454c6468d796128695d37bf5be042c7
fj <castelloiv@gmail.com>
2020-11-02 15:06:25 -0500

    Error hunting

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 import { parse } from "path"
 
-interface Iconfig {
-  port: string
-}
-
-export const config: Iconfig = {
+import * as Interface from "./types/interfaces"
+export const config: Interface.Iconfig = {
   port: process.env.PORT || "7654",
+  redis: {
+    host: process.env.REDIS__HOST || "0.0.0.",
+    port:
+      (process.env.REDIS__PORT && parseInt(process.env.REDIS__HOST)) || 6401,
+  },
 }
