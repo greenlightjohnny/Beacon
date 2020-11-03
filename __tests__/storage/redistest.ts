c63a9205c7f52c2cbea984fd3c888dd8837dd93b
fj <castelloiv@gmail.com>
2020-11-03 15:26:07 -0500

    Mind bent. No more Jest today

--- __tests__/storage/redistest.ts
+++ __tests__/storage/redistest.ts
@@ -1,14 +1,39 @@
 import { redisStorage } from "../../src/storage/redis"
 
 describe("storage/redis", () => {
-  describe("get", () => {
+  xdescribe("get", () => {
+    const list_name = "my_test_list"
     it("should return empty list", async () => {
-      const list_name = "my_test_list"
+      expect(await redisStorage.get(list_name)).toEqual([])
+    })
+
+    it("should return all items in a list", async () => {
       expect(await redisStorage.get(list_name)).toEqual([])
     })
   })
 
-  xdescribe("add", () => {})
+  xdescribe("add", () => {
+    const list_name = "my_test_list"
+    it("should allow adding an element to a list", async () => {
+      expect(await redisStorage.add(list_name, "chris")).toBeTruthy()
 
-  xdescribe("remove", () => {})
+      expect(await redisStorage.get(list_name)).toEqual(["chris"])
+    })
+  })
+
+  xdescribe("remove", () => {
+    const list_name = "my_test_list"
+    it("should allow removing an element from a list", async () => {
+      const name1 = "chris"
+      const name2 = "paul"
+      await redisStorage.add(list_name, name1)
+      await redisStorage.add(list_name, name2)
+
+      expect(await redisStorage.get(list_name)).toEqual([name1, name2])
+
+      expect(await redisStorage.remove(list_name, name1)).toBeTruthy()
+
+      expect(await redisStorage.get(list_name)).toEqual([name2])
+    })
+  })
 })
